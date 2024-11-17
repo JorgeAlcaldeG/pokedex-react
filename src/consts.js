073b3e7017f4ff397c55a9afd6c7df6efe977a3e
unknown <jorge-2806@hotmail.com>
2024-11-17 18:22:12 +0100

    Cambio en consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,7 @@
 
 export const URL = {
-    APIURL:'http://localhost/www/react/pokedex/api/',
+    // APIURL:'http://localhost/www/react/pokedex/api/',
+    APIURL:'http://localhost/proyectos/pokedex-react/api/',
     IMGURL:'../resources/img/',
     FAVURL:'../resources/icon/',
     MAINURL:'../resources/interfaz/mainIcon.png',
